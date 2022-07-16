c0950c01143f795219ff62b3a1db7cbd34a55e68
wilig <will.groppe@gmail.com>
2022-07-16 11:45:08 -0400

    A "working" compiler..
    Creates LLVM IR code in the hackiest fashion available.
    Can read in a single line source file and generate an ELF executable.

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const fs = std.fs;
 const File = fs.File;
+const parser = @import("parser.zig");
 
 pub fn println(comptime msg: []const u8) void {
     std.debug.print(msg, .{});
@@ -19,7 +20,7 @@ pub fn toAbsolutePath(allocator: std.mem.Allocator, path: []const u8) ![]const u
     return try fs.path.resolve(allocator, &[_][]const u8{ cwd, path });
 }
 
-pub fn getFileContents(allocator: std.mem.Allocator, path: []const u8) ![]u8 {
+pub fn getFileContents(allocator: std.mem.Allocator, path: []const u8) ![]const u8 {
     const full_path = try toAbsolutePath(allocator, path);
     defer allocator.free(full_path);
     const src_file = try fs.openFileAbsolute(full_path, .{});
@@ -38,9 +39,9 @@ pub fn getFileContents(allocator: std.mem.Allocator, path: []const u8) ![]u8 {
     return buffer;
 }
 
-pub fn main() void {
+pub fn main() !void {
     // Setup an allocator with all the safety and logging flags set.
-    var gpa = std.heap.GeneralPurposeAllocator(.{ .safety = true, .enable_memory_limit = true, .never_unmap = true, .retain_metadata = true, .verbose_log = true }){};
+    var gpa = std.heap.GeneralPurposeAllocator(.{ .safety = true, .enable_memory_limit = true, .never_unmap = true, .retain_metadata = true }){};
     defer _ = gpa.deinit();
     gpa.setRequestedMemoryLimit(1000000);
     var allocator = gpa.allocator();
@@ -59,6 +60,7 @@ pub fn main() void {
         return;
     }
 
+    var start = std.time.milliTimestamp();
     const src = getFileContents(allocator, args[1]) catch |err| {
         switch (err) {
             error.FileNotFound => vprintln("File '{s}' does not exist.", .{args[1]}),
@@ -67,8 +69,71 @@ pub fn main() void {
         }
         return;
     };
+    vprintln("Loading file took {}ms", .{std.time.milliTimestamp() - start});
     defer allocator.free(src);
-    println("I was able to read the file!");
-    println("Look, here it is:");
-    vprintln("{s}\n", .{src});
+    start = std.time.milliTimestamp();
+    var ast_node = parser.parse(allocator, parser.tokenize(src), 0);
+    defer ast_node.deinit(allocator);
+    vprintln("Parsing took {}ms", .{std.time.milliTimestamp() - start});
+    start = std.time.milliTimestamp();
+    var llvm_ir = parser.LLVM_IR.init(allocator);
+    const llvm_ir_code = llvm_ir.generate(ast_node) catch |err| {
+        vprintln("Failed to generate LLVM ir code. Error: {}", .{err});
+        return;
+    };
+    defer allocator.free(llvm_ir_code);
+    vprintln("LLVM IR generation took {}ms", .{std.time.milliTimestamp() - start});
+
+    start = std.time.milliTimestamp();
+    var llc = std.ChildProcess.init(&[_][]const u8{"llc"}, allocator);
+    llc.stdin_behavior = .Pipe;
+    llc.stdout_behavior = .Pipe;
+    llc.stderr_behavior = .Pipe;
+    llc.spawn() catch {
+        println("Failed to spawn llc");
+        return;
+    };
+    _ = llc.stdin.?.writer().writeAll(llvm_ir_code) catch {
+        println("Failed to write to stdin of process");
+        return;
+    };
+    llc.stdin.?.close();
+    // Critical for unknown reasons..
+    llc.stdin = null;
+
+    var llc_out = try llc.stdout.?.reader().readAllAlloc(allocator, std.math.maxInt(usize));
+    defer allocator.free(llc_out);
+
+    switch (try llc.wait()) {
+        .Exited => |code| if (code != 0) {
+            println("Bad exit code from llc");
+        },
+        else => unreachable,
+    }
+    vprintln("LLC translating took {}ms", .{std.time.milliTimestamp() - start});
+
+    start = std.time.milliTimestamp();
+    var gcc = std.ChildProcess.init(&[_][]const u8{ "gcc", "-o", "a.out", "-xassembler", "-" }, allocator);
+    gcc.stdin_behavior = .Pipe;
+    gcc.stdout_behavior = .Pipe;
+    gcc.stderr_behavior = .Pipe;
+    gcc.spawn() catch {
+        println("Failed to spawn gcc");
+        return;
+    };
+    _ = gcc.stdin.?.writer().writeAll(llc_out) catch {
+        println("Failed to write to stdin of gcc process");
+        return;
+    };
+    gcc.stdin.?.close();
+    // Critical for unknown reasons..
+    gcc.stdin = null;
+
+    switch (try gcc.wait()) {
+        .Exited => |code| if (code != 0) {
+            println("Bad exit code from gcc");
+        },
+        else => unreachable,
+    }
+    vprintln("GCC ELF encoding took {}ms", .{std.time.milliTimestamp() - start});
 }
--- src/parser.zig
+++ src/parser.zig
@@ -82,7 +82,7 @@ fn determineType(value: []const u8) TokenType {
     }
 }
 
-fn parse(allocator: std.mem.Allocator, tokens: *mem.TokenIterator(u8), min_prec: u8) *AstNode {
+pub fn parse(allocator: std.mem.Allocator, tokens: *mem.TokenIterator(u8), min_prec: u8) *AstNode {
     const token = tokens.next() orelse panic("Out of tokens!", .{});
     var tokenType: TokenType = determineType(token);
     var lhs: *AstNode = switch (tokenType) {
@@ -112,6 +112,133 @@ fn parse(allocator: std.mem.Allocator, tokens: *mem.TokenIterator(u8), min_prec:
 
     return lhs;
 }
+
+const LLVMTag = enum { op, val };
+
+const LLVMNode = struct {
+    const Self = @This();
+    allocator: std.mem.Allocator,
+    value: []const u8,
+    reg_num: u32,
+    kind: LLVMTag,
+    lhs: ?*LLVMNode = null,
+    rhs: ?*LLVMNode = null,
+
+    pub fn init(allocator: std.mem.Allocator, kind: LLVMTag, value: []const u8, reg_num: u32) !*Self {
+        var instance = try allocator.create(Self);
+        errdefer allocator.destroy(instance);
+        instance.* = .{ .allocator = allocator, .value = value, .kind = kind, .reg_num = reg_num };
+        return instance;
+    }
+
+    pub fn deinit(self: *Self) void {
+        if (self.lhs) |lhs| {
+            lhs.deinit();
+        }
+        if (self.rhs) |rhs| {
+            rhs.deinit();
+        }
+        self.allocator.destroy(self);
+    }
+};
+
+pub const LLVM_IR = struct {
+    const Self = @This();
+
+    const prelude =
+        \\; ModuleID = 'int.ika'
+        \\source_filename = "int.ika"
+        \\target datalayout = "e-m:e-p270:32:32-p271:32:32-p272:64:64-i64:64-f80:128-n8:16:32:64-S128"
+        \\target triple = "x86_64-redhat-linux-gnu"
+        \\
+        \\@.str = private unnamed_addr constant [12 x i8] c"Answer: %d\0A\00", align 1
+        \\
+        \\define dso_local i32 @main() #0 {
+        \\
+    ;
+
+    const postfix =
+        \\  ret i32 0
+        \\}
+        \\
+        \\declare dso_local i32 @printf(i8* noundef, ...) #1
+        \\
+        \\!0 = !{i32 1, !"wchar_size", i32 4}
+        \\!1 = !{i32 7, !"uwtable", i32 1}
+        \\!2 = !{i32 7, !"frame-pointer", i32 2}
+        \\!3 = !{!"ika version 0.0.1 (Linux)"}
+    ;
+
+    allocator: std.mem.Allocator,
+    reg_counter: u32 = 0,
+
+    pub fn init(alloc: std.mem.Allocator) Self {
+        return Self{
+            .allocator = alloc,
+        };
+    }
+
+    pub fn generate(self: *Self, root: *AstNode) ![]u8 {
+        var llvm_ir = std.ArrayList(u8).init(self.allocator);
+        defer llvm_ir.deinit();
+        try llvm_ir.appendSlice(LLVM_IR.prelude);
+        const llvm_root_node = try self.translate(root);
+        defer llvm_root_node.deinit();
+        const reg = self.appendNode(&llvm_ir, llvm_root_node);
+        const println = try std.fmt.allocPrint(self.allocator, "%noop = call i32 (i8*, ...) @printf(i8* noundef getelementptr inbounds ([12 x i8], [12 x i8]* @.str, i64 0, i64 0), i32 noundef %IKA{})\n", .{reg});
+        defer self.allocator.free(println);
+        try llvm_ir.appendSlice(println);
+        try llvm_ir.appendSlice(LLVM_IR.postfix);
+        return llvm_ir.toOwnedSlice();
+    }
+
+    fn appendNode(self: *Self, buf: *std.ArrayList(u8), node: *LLVMNode) !u32 {
+        var buffer: [100]u8 = std.mem.zeroes([100]u8);
+        switch (node.kind) {
+            .op => {
+                const lhs_reg = self.appendNode(buf, node.lhs.?);
+                const rhs_reg = self.appendNode(buf, node.rhs.?);
+                try buf.appendSlice(try std.fmt.bufPrint(&buffer, "%IKA{} = {s} %IKA{}, %IKA{}\n\n", .{ node.reg_num, node.value, lhs_reg, rhs_reg }));
+                return node.reg_num;
+            },
+            .val => {
+                try buf.appendSlice(try std.fmt.bufPrint(&buffer, "%IKA{} = alloca i32, align 4\n", .{node.reg_num}));
+                try buf.appendSlice(try std.fmt.bufPrint(&buffer, "store i32 {s}, i32* %IKA{}, align 4\n", .{ node.value, node.reg_num }));
+                try buf.appendSlice(try std.fmt.bufPrint(&buffer, "%IKA{} = load i32, i32* %IKA{}, align 4\n\n", .{ node.reg_num + 1, node.reg_num }));
+                return node.reg_num + 1;
+            },
+        }
+    }
+
+    fn translate(self: *Self, node: *AstNode) !*LLVMNode {
+        self.reg_counter += 1;
+        if (node._type != TokenType.atom) {
+            const value =
+                switch (node._type) {
+                TokenType.addition => "add nsw i32",
+                TokenType.subtraction => "sub i32",
+                TokenType.multiplication => "mul nsw i32",
+                TokenType.division => "sdiv i32",
+                else => unreachable,
+            };
+            var llvm_node = try LLVMNode.init(self.allocator, LLVMTag.op, value, self.reg_counter);
+            if (node.lhs) |left| {
+                llvm_node.lhs = self.translate(left) catch unreachable;
+            }
+            if (node.rhs) |right| {
+                llvm_node.rhs = self.translate(right) catch unreachable;
+            }
+
+            return llvm_node;
+        } else {
+            var llvm_node = try LLVMNode.init(self.allocator, LLVMTag.val, node.value, self.reg_counter);
+            // Leave room for the load register
+            self.reg_counter += 1;
+
+            return llvm_node;
+        }
+    }
+};
 
 test "simple numeric parsing tests" {
     const alloc = std.testing.allocator;
@@ -143,4 +270,52 @@ test "simple numeric parsing tests" {
     var fourthSexp = try fourth.toSexp(alloc);
     defer alloc.free(fourthSexp);
     try testing.expect(mem.eql(u8, fourthSexp, "(+ 1 (- (* 5 (/ 8 2)) 12))"));
+}
+
+test "simple llvm ir code generation" {
+    const expected_result =
+        \\; ModuleID = 'int.ika'
+        \\source_filename = "int.ika"
+        \\target datalayout = "e-m:e-p270:32:32-p271:32:32-p272:64:64-i64:64-f80:128-n8:16:32:64-S128"
+        \\target triple = "x86_64-redhat-linux-gnu"
+        \\
+        \\@.str = private unnamed_addr constant [12 x i8] c"Answer: %d\0A\00", align 1
+        \\
+        \\define dso_local i32 @main() #0 {
+        \\%IKA3 = alloca i32, align 4
+        \\store i32 10, i32* %IKA3, align 4
+        \\%IKA4 = load i32, i32* %IKA3, align 4
+        \\
+        \\%IKA5 = alloca i32, align 4
+        \\store i32 3, i32* %IKA5, align 4
+        \\%IKA6 = load i32, i32* %IKA5, align 4
+        \\
+        \\%IKA2 = sdiv i32 %IKA4, %IKA6
+        \\
+        \\%IKA7 = alloca i32, align 4
+        \\store i32 100, i32* %IKA7, align 4
+        \\%IKA8 = load i32, i32* %IKA7, align 4
+        \\
+        \\%IKA1 = add nsw i32 %IKA2, %IKA8
+        \\
+        \\%noop = call i32 (i8*, ...) @printf(i8* noundef getelementptr inbounds ([12 x i8], [12 x i8]* @.str, i64 0, i64 0), i32 noundef %IKA1)
+        \\  ret i32 0
+        \\}
+        \\
+        \\declare dso_local i32 @printf(i8* noundef, ...) #1
+        \\
+        \\!0 = !{i32 1, !"wchar_size", i32 4}
+        \\!1 = !{i32 7, !"uwtable", i32 1}
+        \\!2 = !{i32 7, !"frame-pointer", i32 2}
+        \\!3 = !{!"ika version 0.0.1 (Linux)"}
+    ;
+
+    const alloc = std.testing.allocator;
+    var stmt = "10 / 3 + 100";
+    var ast_node = parse(alloc, tokenize(stmt), 0);
+    defer ast_node.deinit(alloc);
+    var llvm_ir = LLVM_IR.init(alloc);
+    const ir_code = try llvm_ir.generate(ast_node);
+    defer alloc.free(ir_code);
+    try std.testing.expect(std.mem.eql(u8, ir_code, expected_result));
 }
